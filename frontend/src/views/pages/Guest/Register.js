e7d4b6f481d346473edb701ccd9d57be1c0dcbfc
streamdotmysolutions@gmail.com <streamdotmysolutions@gmail.com>
2024-09-12 17:41:14 +0800

    [FRONTEND] register form

--- frontend/src/views/pages/Guest/Register.js
+++ frontend/src/views/pages/Guest/Register.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import { Col,Button,Form }  from 'react-bootstrap';
 
-const Register = () => {
-    return (
-        <div>
-            Register
-        </div>
-    );
-};
+function Register() {
+  return (
+    <Col className='col-6 border border-1 p-4 rounded'>
+        <h1>Register</h1>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+           
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+
+    </Col>
+
+  );
+}
 
 export default Register;
