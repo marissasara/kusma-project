52dbd2f995704826bce6c4e4aa933544bb0d4af0
streamdotmysolutions@gmail.com <streamdotmysolutions@gmail.com>
2024-09-19 09:54:47 +0800

    [FRONTEND] SignIn redirect mock up

--- frontend/src/views/pages/Guest/SignIn.js
+++ frontend/src/views/pages/Guest/SignIn.js
@@ -3,19 +3,26 @@ import { Row, Col, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 import useStore from '../../../store';
 import { appendFormData, InputText } from '../../../FormInput';
+import { Navigate,Link } from 'react-router-dom'
+
 function SignIn() {
     const store = useStore()
+    
 
     const [isLoading, setIsLoading] = useState(false);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState('');
+    
 
     useEffect(() => {
         // Code to run when the component is loaded (similar to window.onload)
         console.log("Page has loaded!");
+
+        // Zustand presets
         store.emptyData() // clear all previous data
+        store.setValue('authenticated', false) // set authenticated to false
 
         // Optionally, you can return a cleanup function to run when the component is unmounted
         return () => {
@@ -24,6 +31,7 @@ function SignIn() {
     }, []); // Empty dependency array means this runs only once, when the component loads
 
 
+    // handle form submission
     const handleSubmit = (e) => {
         setIsLoading(true)
         e.preventDefault();
@@ -49,11 +57,10 @@ function SignIn() {
         })
         .then(response => {
             console.log(response);
+            store.setValue('authenticated', true)
             console.log('Form submitted successfully!');
         })
         .catch(error => {
-   
-
             if( error.response?.status == 422 ){ // detect 422 errors by Laravel
                 console.log(error.response.data.errors)
                 store.setValue('errors', error.response.data.errors ) // set the errors to store
@@ -64,9 +71,14 @@ function SignIn() {
         })
     };
 
+    // handle redirect after login
+    if( store.getValue('authenticated') === true) {
+        return <Navigate to='/dashboard' replace />
+    }
+
     return (
         <Row className='ms-4 col-8 border border-1 p-4 rounded'  style={{ backgroundColor: isLoading ? '#eaeaea' : 'transparent' }} >
-        <h1>Sign In</h1>
+        <h1>Sign In {store.getValue('authenticated') ? 'auth' : 'false'}</h1>
         <Form onSubmit={handleSubmit}>
 
             <Row>
