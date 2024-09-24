2919b83b7f108e8382bdacb0060de19bc819000e
streamdotmysolutions@gmail.com <streamdotmysolutions@gmail.com>
2024-09-24 10:41:41 +0800

    [FRONTEND] GuestLayout

--- frontend/src/views/layouts/Footer.js
+++ frontend/src/views/layouts/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-    <footer class="row bg-light py-4 mt-auto">
+    <footer class="row bg-light py-4 mt-auto bg-dark text-light rounded">
         <div class="col">
-            Copyright 2024 Kusma Membership System
+            Copyright 2024 <strong>NASIONAL FM</strong>
         </div>
     </footer>
     );
--- frontend/src/views/layouts/GuestLayout.js
+++ frontend/src/views/layouts/GuestLayout.js
@@ -2,20 +2,38 @@
 import { Outlet } from 'react-router-dom';
 import SideBar from './SideBar/GuestSideBar';
 import Footer from './Footer';
+import Logo from './nasional-fm.png';
+import Podcast from './rtm-podcast-icon.svg';
 
 const GuestLayout = () => {
   return (
-  <div class="row vh-100 overflow-auto">
-    <SideBar />  
-    <div class="col d-flex flex-column h-sm-100">
-        <main class="row overflow-auto">
-            <div class="col pt-4">
-                <Outlet />
-            </div>
-        </main>
-        <Footer />
-    </div>
-</div>
+    <>
+    <nav class="navbar" style={{
+                                'minHeight': '100px',
+                                'backgroundColor': 'lightcyan'
+                              }}>
+      <span class="navbar-brand mb-0 h1 ms-4">
+        <img src={Logo} alt="Logo" height="60" className="d-inline-block align-text-top"  />
+      </span>
+
+      <span>
+        <img src={Podcast} alt="Podscast" height="60" className="d-inline-block align-text-top me-2"  />
+      </span>
+   
+    </nav>
+
+    <div class="container-fluid row overflow-auto">
+      
+      <div class="col d-flex flex-column h-sm-100">
+          <main class="row overflow-auto mb-4">
+              <div class="col pt-4">
+                  <Outlet />
+              </div>
+          </main>
+          <Footer />
+      </div>
+  </div>
+</>
   );
 };
 
