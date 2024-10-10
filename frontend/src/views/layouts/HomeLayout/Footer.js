62152564d38fbe0fb5d9779c13d972d97faec8d4
StreamDotMy <streamdotmysolutions@gmail.com>
2024-10-10 12:00:31 +0800

    [Frontend] HomeLayout & Footer

--- frontend/src/views/layouts/HomeLayout/Footer.js
+++ frontend/src/views/layouts/HomeLayout/Footer.js
@@ -1,11 +1,72 @@
 import React from 'react';
 import SocialMediaIcons from './SocialMediaIcons';
+import ksn_logo from './ksn2.png';
+import mampu_logo from './mampu.png';
+import mygov_logo from './mygov2.png';
+import msc_logo from './msc-white.png';
+import apps_logo from './apps.png';
+import play_logo from './googleplay-bm.png';
+import huawei_logo from './huawei_appsgaleryedit1.png';
 
 const Footer = () => {
     return (
-    <footer className="mt-4 py-4 text-dark rounded" style={{'backgroundColor' : 'lightcyan'}}>
-        <div className="col">
-            <SocialMediaIcons />
+    // <footer className="mt-4 py-4 text-dark rounded" style={{'backgroundColor' : 'lightcyan'}}>
+    //     <div className="col">
+    //         <SocialMediaIcons />
+    //     </div>
+    // </footer>
+    <footer className="text-light rounded" style={{'backgroundColor': '#000'}}>
+   
+            <div className="row justify-content-center p-5 ms-5">
+
+                <div className="col-3">
+                    <ul className='list-unstyled'>
+                        <li className='mb-2'><a href="http://www.mampu.gov.my/ms/"><img className="img-fluid" src={mampu_logo} alt="Mampu Logo" /></a></li>
+                        <li className='mb-2'><a href="https://www.malaysia.gov.my/portal/index"><img className="img-fluid" src={mygov_logo} alt="MyGov Logo" /></a></li>
+                        <li className='mb-2'><a href="#"><img className="img-fluid" src={ksn_logo} alt="KSN Logo" /></a></li>
+                        <li><a href="#"><img className="img-fluid" src={msc_logo} alt="MSC Logo" /></a></li>
+                    </ul>
+                </div>
+
+                
+                <div className="col-3">
+                    <ul className='list-unstyled'>
+                        <li><a className="text-light" href="http://www.mampu.gov.my/ms/">Dasar Keselamatan</a></li>
+                        <li><a className="text-light" href="http://www.mampu.gov.my/ms/">Dasar Privasi</a></li>
+                        <li><a className="text-light" href="http://www.mampu.gov.my/ms/">Latar Belakang</a></li>
+                        <li><a className="text-light" href="http://www.mampu.gov.my/ms/">Mengenai Kami</a></li>
+
+                    </ul>
+                </div>
+
+
+    
+
+                <div className="col-3">
+                    <p>Radio Televisyen Malaysia<br />
+                        Angkasapuri Kota Media<br />
+                        50614 Kuala Lumpur</p>
+                    <p>aduan [at] rtm.gov.my </p>
+                    <p> Tel: 03 - 2282 5333 </p>
+                    <p> Faks: 03 - 2284 7591 </p>
+                </div>
+
+                <div className="col-3 text-center ">
+                    <ul className="list-unstyled ">
+                        <li className='mb-2'><a href="https://apps.apple.com/my/app/rtmklik/id777391399"><img className="img-fluid" src={apps_logo} alt="Apple App Store Logo" /></a></li>
+                        <li className='mb-2'><a href="https://play.google.com/store/apps/details?id=my.gov.rtm.mobile"><img className="img-fluid" src={play_logo} alt="Google Play Store Logo" /></a></li>
+                        <li className='mb-1'><a href="https://appgallery.huawei.com/app/C101841473"><img className="img-fluid" src={huawei_logo} alt="Huawei AppGallery Logo" /></a></li>
+                    </ul>
+                </div>
+          
+        </div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 d-flex justify-content-center">
+                    <p style={{ marginTop: "0.5rem" }}>Hak Cipta Terpelihara @ {new Date().getFullYear()} RADIO TELEVISYEN MALAYSIA</p>
+                </div>
+            </div>
         </div>
     </footer>
     );
--- frontend/src/views/layouts/HomeLayout/HomeLayout.js
+++ frontend/src/views/layouts/HomeLayout/HomeLayout.js
@@ -3,13 +3,27 @@ import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Logo from './nasional-fm.png';
 import Podcast from './rtm-podcast-icon.png';
-
+import { useEffect } from 'react';
 const HomeLayout = () => {
+
+
+  useEffect(() => {
+    // Apply background color to the body element
+    document.body.style.backgroundColor = 'lightcyan';
+
+    // Cleanup when the component unmounts (optional)
+    return () => {
+      document.body.style.backgroundColor = '';
+    };
+  }, []);
+
+
   return (
     <>
-    <nav className="navbar" style={{
+
+    <nav className="navbar rounded" style={{
                                 'minHeight': '100px',
-                                'backgroundColor': 'lightcyan'
+                                'backgroundColor': 'cyan'
                               }}>
       <span className="navbar-brand mb-0 h1 ms-4">
         <img src={Logo} alt="Logo NasionalFM" height="60" className="d-inline-block align-text-top"  />
@@ -21,7 +35,7 @@ const HomeLayout = () => {
    
     </nav>
 
-    <div className="container-fluid row overflow-auto">
+    <div className="container-fluid row overflow-auto " style={{'backgroundColor':'lightcyan'}}>
       
       <div className="col d-flex flex-column h-sm-100 mb-2 mt-4">
           <Outlet />
