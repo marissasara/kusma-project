fb0deeb35234c3223e827b0a5461c74b996b0e1d
StreamDotMy <streamdotmysolutions@gmail.com>
2024-10-13 13:04:28 +0800

    [Frontend] Choice Preview Image

--- frontend/src/views/pages/Admin/Choices/components/HtmlFormComponent.js
+++ frontend/src/views/pages/Admin/Choices/components/HtmlFormComponent.js
@@ -1,4 +1,4 @@
-import React, {  } from 'react'
+import React, { useEffect, useState } from 'react'
 import { InputText,InputFile, InputRadio, InputDate, InputTextarea } from '../../../../../libs/FormInput';
 import { Form,Row,Col, Image, Figure, FormGroup } from 'react-bootstrap';
 import useStore from '../../../../../store'
@@ -6,6 +6,15 @@ import useStore from '../../../../../store'
 const HtmlFormComponent = ({isLoading}) => {
     const store = useStore();
     const url = process.env.REACT_APP_SERVER_URL; 
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    useEffect( () => {
+        const file = store.getValue('photo')
+        if (file) {
+            setSelectedImage(URL.createObjectURL(file));
+        }
+    })
+
     return (
         <>
  
@@ -15,7 +24,6 @@ const HtmlFormComponent = ({isLoading}) => {
                     fieldName='title' 
                     placeholder='Title'  
                     icon='fa-solid fa-pencil'
-           
                     isLoading={isLoading}
                 />
 
@@ -30,6 +38,31 @@ const HtmlFormComponent = ({isLoading}) => {
                     isLoading={isLoading}
                 />
 
+            </Col>
+
+             
+            <hr />
+            <h5>Poster</h5>
+            <Col className='mb-2'>
+                {store.getValue('filename')  || selectedImage ? (
+
+                    <Figure>
+                         <Figure.Image
+                            className="img-fluid"
+                            src={selectedImage || `${url}/storage/choices/${store.getValue('filename')}`}
+                            alt="Preview"
+                        />
+                    </Figure>
+                          
+                ) : (
+                    <InputFile
+                        fieldName='photo' 
+                        placeholder='Choose image'  
+                        icon='fa-solid fa-image'
+                        isLoading={isLoading}
+                  
+                    />
+                )}
             </Col>
             
           
