f795e3b14a3c419ff2fd0765d96ed2cb7dce0189
StreamDotMy <streamdotmysolutions@gmail.com>
2024-10-15 22:04:39 +0800

    [AZRIL] Deejay social media link

--- frontend/src/views/pages/Home/Home.js
+++ frontend/src/views/pages/Home/Home.js
@@ -17,22 +17,22 @@ const Home = () => {
                 <BannerComponent />
             </Row>
 
-            <Row className="mt-4 mb-4 ms-1">
+            <Row className="mt-4 mb-4 ms-1 bg-info pt-4 pb-3 rounded">
                 {/* <CardComponent /> */}
                 <DeejayComponent />
             </Row>
             <hr />
-            <Row className="mt-4 mb-4 ms-1">
-                <Col  xs={12} md={8} className="border border-1 p-3 rounded mb-3">
+            <Row className="mt-4 mb-4 ms-1  pt-4 pb-3 bg-dark rounded ">
+                <Col  xs={12} md={8} className=" p-3 rounded mb-3">
                     <VideoComponent />
                 </Col>
 
-                <Col xs={12} md={4} className="border border-1 p-3 rounded d-none d-sm-flex">
+                <Col xs={12} md={4} className=" p-3 rounded d-none d-sm-flex">
                     <ChatroomComponent />
                 </Col>
             </Row>
 
-            <Row className="mt-4 mb-4 ms-1">
+            <Row className="mt-4 mb-4 ms-1 pt-4 pb-3">
                 {/* <CardComponent /> */}
                 <VoteComponent />
             </Row>
--- frontend/src/views/pages/Home/components/ChatroomComponent.js
+++ frontend/src/views/pages/Home/components/ChatroomComponent.js
@@ -4,7 +4,7 @@ const ChatroomComponent = () => {
     return (
         <div style={{'width':'100%'}}>
             <h2>Chatroom</h2>
-            <iframe src="https://deadsimplechat.com/3Nmi1rTcx" width="100%" height="600px"></iframe>
+            <iframe src="https://deadsimplechat.com/3Nmi1rTcx" width="100%" style={{'minHeight': '535px'}}></iframe>
         </div>
     );
 };
