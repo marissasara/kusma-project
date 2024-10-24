94bfea3f0747f446df76bd5ce3923a3284d28083
StreamDotMy <streamdotmysolutions@gmail.com>
2024-10-24 19:31:01 +0800

    [HomeLayout] font google

--- frontend/src/views/layouts/HomeLayout/HomeLayout.js
+++ frontend/src/views/layouts/HomeLayout/HomeLayout.js
@@ -19,9 +19,38 @@ const HomeLayout = () => {
     };
   }, []);
 
+  useEffect(() => {
+    // Create link elements
+    const link1 = document.createElement('link');
+    link1.rel = 'preconnect';
+    link1.href = 'https://fonts.googleapis.com';
+    
+    const link2 = document.createElement('link');
+    link2.rel = 'preconnect';
+    link2.href = 'https://fonts.gstatic.com';
+    link2.crossOrigin = 'true';
+    
+    const link3 = document.createElement('link');
+    link3.href = 'https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap';
+    link3.rel = 'stylesheet';
+
+    // Append links to the head of the document
+    document.head.appendChild(link1);
+    document.head.appendChild(link2);
+    document.head.appendChild(link3);
+
+    return () => {
+      // Cleanup: remove the links when the component unmounts
+      document.head.removeChild(link1);
+      document.head.removeChild(link2);
+      document.head.removeChild(link3);
+    };
+  }, []);
+
 
   return (
     <>
+    
 
     {/* <nav className="navbar" style={{
                                 'minHeight': '100px',
